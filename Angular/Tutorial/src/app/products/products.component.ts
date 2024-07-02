0a3c96e029e83c301bb5bc1fc5a14679bcbe3bf9
nambiiina <rthierrynambinina@gmail.com>
2024-07-02 16:27:57 +0300

    - Data binding - json server - http

--- Angular/Tutorial/src/app/products/products.component.ts
+++ Angular/Tutorial/src/app/products/products.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
+  products: Array<any> = []
+
+  constructor(private http:HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<Array<any>>('http://localhost:8080/products').subscribe({
+      next: data => {
+        this.products = data;
+        debugger;
+      },
+      error: error => {
+        console.error(error);
+      }
+    })
+  }
+
+  handleCheckProduct(product: any) {
+    this.http.patch(
+      `http://localhost:8080/products/${product.id}`,
+      {checked:!product.checked}
+    ).subscribe({
+      next: updatedProduct => {
+        product.checked = !product.checked;
+      },
+    });
+  }
 }
